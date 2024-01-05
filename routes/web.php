4a70b5273d139dfe5b30132da63aacc7973c7444
adrianC05 <aacarrion4@espe.edu.ec>
2024-01-04 20:07:55 -0500

    Implementacion de panel administrativo adminLTE

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::group(['prefix'=> 'admin'],function(){
+
+Route::get('Panel-Administrativo', [IndexController::class, 'index'])->name('dashboard');
+
 });
+
